b35a79e78c572d9bf25c03b886aeae101ce454c7
Shreyamkmr <shreyamkmr@gmail.com>
2020-09-07 17:08:21 +0530

    followed the changes

--- js/main.js
+++ js/main.js
@@ -234,7 +234,7 @@ btn.on('click', function(e) {
 	};
 	scrollWindow();
 
-
+	
 	var counter = function() {
 		
 		$('#section-counter, .hero-wrap, .ftco-counter').waypoint( function( direction ) {
@@ -246,22 +246,50 @@ btn.on('click', function(e) {
 					var $this = $(this),
 						num = $this.data('number');
 						console.log(num);
+
 					$this.animateNumber(
 					  {
+
 					    number: num,
 					    numberStep: comma_separator_number_step
-					  }, 7000
+
+					  }, 3000
 					);
 				});
 				
 			}
 
-		} , { offset: '95%' } );
+
+		} , { offset: '-100%' } );
 
 	}
+
 	counter();
 
 
+// below listed default settings
+AOS.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: 'aos-init', // class applied after initialization
+  animatedClassName: 'aos-animate', // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+  
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
 	var contentWayPoint = function() {
 		var i = 0;
 		$('.ftco-animate').waypoint( function( direction ) {
