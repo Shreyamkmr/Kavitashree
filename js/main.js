1740d75338a4eac3503a561d6046c8b5e82e9e63
Shreyamkmr <shreyamkmr@gmail.com>
2020-09-10 18:33:54 +0530

    scrollProperty: 'scroll' removed from slideshow window

--- js/main.js
+++ js/main.js
@@ -73,7 +73,7 @@ setInterval(function() {
     parallaxElements: true,
     horizontalScrolling: false,
     hideDistantElements: false,
-    scrollProperty: 'scroll'
+    
   });
 	//https://codepen.io/chriscoyier/pen/zKbYzP slideshow credits
 
