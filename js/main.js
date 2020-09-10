32463a29983b7f2e6980a78f835a3691d7ccc2fa
Shreyamkmr <shreyamkmr@gmail.com>
2020-09-10 18:21:52 +0530

    scroll to top removed

--- js/main.js
+++ js/main.js
@@ -177,21 +177,6 @@ setInterval(function () {
 	  console.log('show');
 	});
 
-	//back to top
-	var btn = $('#button');
-
-$(window).scroll(function() {
-  if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-  }
-});
-
-btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
-});
 
 
 	// scroll
